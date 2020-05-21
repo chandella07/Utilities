900ffae30685a0dc9c01a12f23bd34b4bc53e248
Chandella <DChandella@INTERNAL.COLT.NET>
2020-05-21 23:31:19 +0530

    clean unused files

--- Capybara-demo/Cucumber_Framework/features/support/env.rb
+++
@@ -1,9 +0,0 @@
-require 'capybara/cucumber'
-require 'selenium/webdriver'
-
-Capybara.default_driver=:selenium
-
-Capybara.register_driver :selenium do |app|                                                                                                                                      
-    Capybara::Selenium::Driver.new(app, :browser => :firefox)
-end 
-
--- add_package_to_pypi/python-pypi/README.md
+++
@@ -1,1 +0,0 @@
-This is a READme file.
